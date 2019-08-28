28d5a8684039c62487534f9fbde1beda28605b9d
Camila <38863912+camilacvieira@users.noreply.github.com>
2019-08-28 15:46:33 -0300

    Inclusão Quicksort Recursivo

--- include/algoritmosOrdenacao.h
+++ include/algoritmosOrdenacao.h
@@ -7,22 +7,22 @@ class algoritmosOrdenacao
     public:
         algoritmosOrdenacao(int *vetor, int tam);
         virtual ~algoritmosOrdenacao();
-        void bubbleSort();
-        void selectionSort();
-        void insertionSort();
+        void bubbleSort();//Todos
+        void selectionSort();//Todos
+        void insertionSort();//Todos
         void heapSort();//VICTOR
         void heapfy(int* arvore,int indice);//VICTOR
         void buildMaxHeap(int* arvore);//VICTOR
         void swap(int *a, int *b);
         void imprime();
         void randomiza();
-        void quickSort();
-        int particao();
+        void quickSort(); //Camila
+        void quickSortAux(int *vetor, int inicio, int fim); //Camila
 
     protected:
 
     private:
-        int *vetor1, tam1;
+        int *vetor1, tam1, inicio1, fim1;
 
 };
 
