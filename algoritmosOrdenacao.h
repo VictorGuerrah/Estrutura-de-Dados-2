9557753ee6a0e886fd36c6e2e9c2dc4b537c0934
Pedro Cotta Badaro <37852363+pedrocottabadaro@users.noreply.github.com>
2019-10-14 11:58:40 -0300

    funcoes quick registro

---
+++ algoritmosOrdenacao.h
@@ -0,0 +1,53 @@
+#ifndef ALGORITMOSORDENACAO_H
+#define ALGORITMOSORDENACAO_H
+#include <iostream>
+#include <vector>
+#include "registro.h"
+using namespace std;
+
+class algoritmosOrdenacao
+{
+    public:
+        algoritmosOrdenacao(vector<int>& vetor, int tam);
+        algoritmosOrdenacao(vector<registro>& vetorregs, int tam);
+        virtual ~algoritmosOrdenacao();
+        void bubbleSort();//Todos
+        void selectionSort();//Todos
+        void insertionSort();//Todos
+        void heapSort();//Victor
+        void heapfy(vector<int>& arvore,int indice);//Victor
+        void buildMaxHeap(vector<int>& arvore);//Victor
+        void swap(int* a, int* b);
+        void imprime();
+        void randomiza();
+        void quickSort(); //Camila
+        void quickSortAux(vector<int>& vetor, int inicio, int fim); //Camila
+
+        void quickSortRegistro();
+        void quickSortAuxRegistro(vector<registro>& vetor, int inicio, int fim);
+
+        void quickSortMediana(); //Camila
+        void quickSortMedianaAux(vector<int>& vetor, int inicio, int fim); //Camila
+        void countSort(); //Victor
+        void auxQuickSortHibrido();//Pedro
+        void QuickSortHibrido(vector<int>& vetor,int inicio,int fim);//Pedro
+        int particaoQuickSort(vector<int>& vetor,int inicio,int fim);//Pedro
+        void mergeSort(vector<int>& vetor, int inicio, int fim); //Daniel
+        void intercala(vector<int>& vetor, int inicio, int meio, int fim); //Daniel
+        void mergeSortAux(); //Daniel
+
+        void zeraTrocas(){trocas = 0;};
+        void zeraComparacoes(){comparacoes = 0;};
+
+	int getComparacoes();
+	int getTrocas();
+    protected:
+
+    private:
+	vector<int> vetor1;
+	vector<registro> vetorregs1;
+        int tam1, inicio1, fim1;
+	int comparacoes,trocas;
+};
+
+#endif // ALGORITMOSORDENACAO_H
