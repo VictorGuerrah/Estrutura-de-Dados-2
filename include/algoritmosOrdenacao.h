fb40df54db71d1c0c850169d569f5b3fae150e60
Pedro Cotta Badaro <37852363+pedrocottabadaro@users.noreply.github.com>
2019-10-14 12:02:56 -0300

    construtor e funcoes do quick registro

--- include/algoritmosOrdenacao.h
+++ include/algoritmosOrdenacao.h
@@ -2,13 +2,14 @@
 #define ALGORITMOSORDENACAO_H
 #include <iostream>
 #include <vector>
-
+#include "registro.h"
 using namespace std;
 
 class algoritmosOrdenacao
 {
     public:
         algoritmosOrdenacao(vector<int>& vetor, int tam);
+        algoritmosOrdenacao(vector<registro>& vetorregs, int tam);
         virtual ~algoritmosOrdenacao();
         void bubbleSort();//Todos
         void selectionSort();//Todos
@@ -21,7 +22,11 @@ class algoritmosOrdenacao
         void randomiza();
         void quickSort(); //Camila
         void quickSortAux(vector<int>& vetor, int inicio, int fim); //Camila
-	    void quickSortMediana(); //Camila
+
+        void quickSortRegistro();
+        void quickSortAuxRegistro(vector<registro>& vetor, int inicio, int fim);
+
+        void quickSortMediana(); //Camila
         void quickSortMedianaAux(vector<int>& vetor, int inicio, int fim); //Camila
         void countSort(); //Victor
         void auxQuickSortHibrido();//Pedro
@@ -30,14 +35,17 @@ class algoritmosOrdenacao
         void mergeSort(vector<int>& vetor, int inicio, int fim); //Daniel
         void intercala(vector<int>& vetor, int inicio, int meio, int fim); //Daniel
         void mergeSortAux(); //Daniel
+
+        void zeraTrocas(){trocas = 0;};
+        void zeraComparacoes(){comparacoes = 0;};
+
 	int getComparacoes();
 	int getTrocas();
-	void zeraTrocas(){trocas = 0;};
-	void zeraComparacoes(){comparacoes = 0;};
     protected:
 
     private:
 	vector<int> vetor1;
+	vector<registro> vetorregs1;
         int tam1, inicio1, fim1;
 	int comparacoes,trocas;
 };
